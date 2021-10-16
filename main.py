ec218da051fc860fb522956e6d12864d20248578
EvilArthas <protracer@mail.ru>
2021-10-16 23:24:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
+import os
+
 from telethon import TelegramClient, sync, events
 
-INPUT_CHANNEL = 'input_channel_username'
-OUTPUT_CHANNEL = 'output_channel_username'
-TAGS = ['#TAG1', '#TAG2']
+inputChannels = ['https://t.me/joinchat/qfyUXdRVvv5lODRi', 'https://t.me/joinchat/MarNjllx6hkxYjVi']
+outputChannels = ['https://t.me/joinchat/mwyF0-LTS340OGYy']
 
 # 1. Заходим на сайт https://my.telegram.org/apps
 # 2. Заполняем поля App title и Short name, нажимаем «Create application» и запоминаем две переменные: api_id и api_hash.
 
-
-api_id = 1234567
-api_hash = 'apihash'
-
+api_id = os.environ['API_ID']
+api_hash = os.environ['API_HASH']
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-
-
-
-@client.on(events.NewMessage(chats=(INPUT_CHANNEL)))
+@client.on(events.NewMessage(chats=inputChannels))
 async def normal_handler(event):
-    for tag in TAGS:
-
-        if tag in str(event.message):
-
-            await client.send_message(OUTPUT_CHANNEL, event.message)
+    for channel in outputChannels:
+        await client.send_message(channel, event.message)
 
 client.start()
 client.run_until_disconnected()

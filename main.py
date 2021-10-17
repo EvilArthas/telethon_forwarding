1497b530d809028ab94d3229cb77b2d226a8f8f4
EvilArthas <protracer@mail.ru>
2021-10-17 14:36:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import json
 import os
 
 from telethon import TelegramClient, sync, events
-
-inputChannels = ['https://t.me/joinchat/qfyUXdRVvv5lODRi', 'https://t.me/joinchat/MarNjllx6hkxYjVi']
-outputChannels = ['https://t.me/joinchat/mwyF0-LTS340OGYy']
+from telethon.tl.types import Channel
 
 # 1. Заходим на сайт https://my.telegram.org/apps
 # 2. Заполняем поля App title и Short name, нажимаем «Create application» и запоминаем две переменные: api_id и api_hash.
 
 api_id = os.environ['API_ID']
 api_hash = os.environ['API_HASH']
+session = os.environ['SESSION']
+inputChannels = json.loads(os.environ['INPUT_CHANNELS'])
+outputChannels = json.loads(os.environ['OUTPUT_CHANNELS'])
 
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient(session, api_id, api_hash)
 
 @client.on(events.NewMessage(chats=inputChannels))
 async def normal_handler(event):
@@ -19,4 +21,9 @@ async def normal_handler(event):
         await client.send_message(channel, event.message)
 
 client.start()
+
+for dialog in client.iter_dialogs():
+    if isinstance(dialog.entity, Channel):
+        print(dialog.entity.title + " - " + str(dialog.entity.id))
+
 client.run_until_disconnected()
